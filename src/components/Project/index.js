24fac1a617a46ae5c7ac2b6cec261c8efc62764a
fame <>
2018-09-20 16:24:49 +0700

    [UPDATE] border img

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import { Container } from 'reactstrap'
+import styled from 'styled-components'
+
+const Image = styled.img`
+  border: 5px solid #fff;
+`
 
 export default class Project extends React.Component {
   render() {
     return (
       <Container>
-        <div className='text-center my-5'>
-          {[1,2,3,4,5,6,7,8,9,10,11,12].map((data,index) => (
-            <img key={index} src={`static/img/posts/${data}.jpg`} className="p-2" />
+        <div className="text-center my-5">
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((data, index) => (
+            <Image
+              key={index}
+              src={`static/img/posts/${data}.jpg`}
+              className="m-2"
+            />
           ))}
         </div>
       </Container>
